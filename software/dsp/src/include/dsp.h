4e9c23f72a7be89ca3c656e41e2eef655d16f359
João Silva <jgc3silva@gmail.com>
2020-07-15 12:32:56 +0100

    Add DSP oscillator

--- software/dsp/src/include/dsp.h
+++ software/dsp/src/include/dsp.h
@@ -5,11 +5,28 @@
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
+#include "arm_math.h"
 #include "iq16.h"
 
+typedef struct dsp_oscillator_t dsp_oscillator_t;
+typedef struct dsp_quad_oscillator_t dsp_quad_oscillator_t;
 typedef struct dsp_am_demod_ctx_t dsp_am_demod_ctx_t;
 typedef struct dsp_fm_demod_ctx_t dsp_fm_demod_ctx_t;
 
+struct dsp_oscillator_t
+{
+    uint32_t ulSteps;
+    uint32_t ulLUTSize;
+    uint32_t ulCurrentPhase;
+    int16_t *psData;
+};
+struct dsp_quad_oscillator_t
+{
+    uint32_t ulSteps;
+    uint32_t ulLUTSize;
+    uint32_t ulCurrentPhase;
+    iq16_t *pData;
+};
 struct dsp_am_demod_ctx_t
 {
 
@@ -24,6 +41,16 @@ struct dsp_fm_demod_ctx_t
 
 uint16_t dsp_abs_int16(int16_t sValue);
 int16_t dsp_atan2_int16(int16_t sX, int16_t sY);
+
+dsp_oscillator_t* dsp_oscillator_init(uint32_t ulSampleRate, int32_t lFreqneucy, uint32_t ulLUTSize);
+void dsp_oscillator_delete(dsp_oscillator_t *pOscillator);
+int16_t dsp_oscillator_get_sample(dsp_oscillator_t *pOscillator, int32_t lPhaseOffset);
+void dsp_oscillator_mix(dsp_oscillator_t *pOscillator, int16_t *psInput, int16_t *psOutput);
+
+dsp_quad_oscillator_t* dsp_quad_oscillator_init(uint32_t ulSampleRate, int32_t lFreqneucy, uint32_t ulLUTSize);
+void dsp_quad_oscillator_delete(dsp_quad_oscillator_t *pOscillator);
+iq16_t dsp_quad_oscillator_get(dsp_quad_oscillator_t *pOscillator, int32_t lPhaseOffset);
+void dsp_quad_oscillator_mix(dsp_quad_oscillator_t *pOscillator, iq16_t *pInput, iq16_t *pOutput);
 
 dsp_am_demod_ctx_t* dsp_am_demod_init();
 void dsp_am_demod_delete(dsp_am_demod_ctx_t *pContext);
