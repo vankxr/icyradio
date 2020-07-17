e35c889a6b62261b799b4d9ddf8ec2ef33024ae6
João Silva <jgc3silva@gmail.com>
2020-07-17 16:43:47 +0100

    Fix oscillator and add FIR checks

--- software/dsp/src/include/dsp.h
+++ software/dsp/src/include/dsp.h
@@ -16,14 +16,12 @@ typedef struct dsp_fm_demod_ctx_t dsp_fm_demod_ctx_t;
 struct dsp_oscillator_t
 {
     uint32_t ulSteps;
-    uint32_t ulLUTSize;
     uint32_t ulCurrentPhase;
     int16_t *psData;
 };
 struct dsp_quad_oscillator_t
 {
     uint32_t ulSteps;
-    uint32_t ulLUTSize;
     uint32_t ulCurrentPhase;
     iq16_t *pData;
 };
@@ -42,15 +40,15 @@ struct dsp_fm_demod_ctx_t
 uint16_t dsp_abs_int16(int16_t sValue);
 int16_t dsp_atan2_int16(int16_t sX, int16_t sY);
 
-dsp_oscillator_t* dsp_oscillator_init(uint32_t ulSampleRate, int32_t lFreqneucy, uint32_t ulLUTSize);
+dsp_oscillator_t* dsp_oscillator_init(uint32_t ulSampleRate, int32_t lFreqneucy);
 void dsp_oscillator_delete(dsp_oscillator_t *pOscillator);
 int16_t dsp_oscillator_get_sample(dsp_oscillator_t *pOscillator, int32_t lPhaseOffset);
-void dsp_oscillator_mix(dsp_oscillator_t *pOscillator, int16_t *psInput, int16_t *psOutput);
+void dsp_oscillator_mix(dsp_oscillator_t *pOscillator, int16_t *psInput, int16_t *psOutput, uint32_t ulSize, int32_t lPhaseOffset);
 
-dsp_quad_oscillator_t* dsp_quad_oscillator_init(uint32_t ulSampleRate, int32_t lFreqneucy, uint32_t ulLUTSize);
+dsp_quad_oscillator_t* dsp_quad_oscillator_init(uint32_t ulSampleRate, int32_t lFreqneucy);
 void dsp_quad_oscillator_delete(dsp_quad_oscillator_t *pOscillator);
 iq16_t dsp_quad_oscillator_get(dsp_quad_oscillator_t *pOscillator, int32_t lPhaseOffset);
-void dsp_quad_oscillator_mix(dsp_quad_oscillator_t *pOscillator, iq16_t *pInput, iq16_t *pOutput);
+void dsp_quad_oscillator_mix(dsp_quad_oscillator_t *pOscillator, iq16_t *pInput, iq16_t *pOutput, uint32_t ulSize, int32_t lPhaseOffset);
 
 dsp_am_demod_ctx_t* dsp_am_demod_init();
 void dsp_am_demod_delete(dsp_am_demod_ctx_t *pContext);
