7786c98e42250298396076d30bf6e20f657f7351
João Silva <jgc3silva@gmail.com>
2020-10-25 23:35:24 +0000

    Extend PMC library Change CPU frequency to 360 MHz (out of spec, confirmed working)

--- software/dsp/src/include/pmc.h
+++ software/dsp/src/include/pmc.h
@@ -17,10 +17,28 @@ extern uint32_t UPLLCK_CLOCK_FREQ;
 extern uint32_t MCK_CLOCK_FREQ;
 extern uint32_t FCLK_CLOCK_FREQ;
 
+#define PMC_PCK_PCK0    0
+#define PMC_PCK_PCK1    1
+#define PMC_PCK_PCK2    2
+#define PMC_PCK_ETM     3
+#define PMC_PCK_UART    4
+#define PMC_PCK_MCAN    5
+#define PMC_PCK_TC      6
+#define PMC_PCK_TC0     7
+
 void pmc_init();
 void pmc_update_clocks();
 
-uint32_t pmc_get_pck_clock_freq(uint8_t ubID);
-uint32_t pmc_get_generic_clock_freq(uint8_t ubPeripheralID);
+void pmc_plla_config(uint8_t ubEnable, uint8_t ubMultiplier, uint8_t ubDivider);
+
+void pmc_usb_clock_config(uint8_t ubUPLLEnable, uint8_t ubUPLLClockDivider, uint8_t ubFSClockEnable, uint8_t ubFSClockSource, uint16_t usFSClockPrescaler);
+
+void pmc_pck_clock_config(uint8_t ubID, uint8_t ubEnable, uint8_t ubSource, uint16_t usPrescaler);
+uint32_t pmc_pck_clock_get_freq(uint8_t ubID);
+
+void pmc_generic_clock_config(uint8_t ubID, uint8_t ubEnable, uint8_t ubSource, uint16_t usPrescaler);
+uint32_t pmc_generic_clock_get_freq(uint8_t ubID);
+
+void pmc_peripheral_clock_gate(uint8_t ubID, uint8_t ubEnable);
 
 #endif  // __PMC_H__
