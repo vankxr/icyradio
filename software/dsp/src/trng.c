9784adc960a72895fc1a5ed5cba96e4e05171993
João Silva <jgc3silva@gmail.com>
2020-10-29 04:28:20 +0000

    Implement new PMC API

--- software/dsp/src/trng.c
+++ software/dsp/src/trng.c
@@ -2,7 +2,7 @@
 
 void trng_init()
 {
-    PMC->PMC_PCR = PMC_PCR_EN | PMC_PCR_CMD | (TRNG_CLOCK_ID << PMC_PCR_PID_Pos); // Enable peripheral clock
+    pmc_peripheral_clock_gate(TRNG_CLOCK_ID, 1); // Enable peripheral clock
 
     TRNG->TRNG_CR = TRNG_CR_KEY_PASSWD | TRNG_CR_ENABLE;
 }
